51149ac127497044534ce5f09e7499c4e5c3d663
harshadme <meharshad00@gmail.com>
2026-01-17 07:28:22 +0530

    Add files via upload

---
+++ app/login/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function login() {
+    setError("");
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+      return;
+    }
+
+    router.push("/");
+  }
+
+  return (
+    <main style={{ padding: 40, maxWidth: 400, margin: "auto" }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ width: "100%", marginBottom: 10 }}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{ width: "100%", marginBottom: 10 }}
+      />
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <button onClick={login} style={{ width: "100%" }}>
+        Login
+      </button>
+    </main>
+  );
+}
